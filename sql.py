d92c6ea403cd10175a01aecc2977914588908f6d
abc0012383 <282404250@qq.com>
2018-03-27 13:40:01 +0800

    Update sql.py

--- sql.py
+++ sql.py
@@ -1,55 +1,51 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+#author Wesker
 
 import sqlite3
-import json
 import smtplib
+from pyecharts import Bar
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 
-conn = sqlite3.connect('use.db')
+conn = sqlite3.connect('/opt/licences/licence_check/use.db')
 c = conn.cursor()
 cursor = c.execute("select * from issued order by rowid desc limit 1;")
 for row in cursor:
-    l1 = list(row)
+    issued_last_record = list(row)
 conn.close()
 
-l2 = [
-    'time','xxx',
-]
-
-l3 = dict(zip(l2, l1))
-
-conn = sqlite3.connect('use.db')
+conn = sqlite3.connect('/opt/licences/licence_check/use.db')
 c = conn.cursor()
-cursor1 = c.execute("select * from used order by rowid desc limit 1;")
-for row1 in cursor1:
-    l4 = list(row1)
+cursor1 = c.execute("select * from issued")
+col_name_list = [tuple[0] for tuple in c.description]
 conn.close()
 
-l5 = dict(zip(l2, l4))
+conn = sqlite3.connect('/opt/licences/licence_check/use.db')
+c = conn.cursor()
+cursor2 = c.execute("select * from used order by rowid desc limit 1;")
+for row1 in cursor2:
+    used_last_record = list(row1)
+conn.close()
 
-json.dump(l3, open('/tmp/issued.txt', 'w'))
-json.dump(l5, open('/tmp/used.txt', 'w'))
+issued_last_record.pop(0)
+col_name_list.pop(0)
+used_last_record.pop(0)
 
-with open('/tmp/issued.txt', 'r') as r:
-    line = r.readlines()
-
-with open('/tmp/issued.txt', 'w') as w:
-    for k in line:
-        w.write(k.replace(',', '\n'))
-
-with open('/tmp/used.txt', 'r') as r:
-    lines = r.readlines()
-
-with open('/tmp/used.txt', 'w') as w:
-    for v in lines:
-        w.write(v.replace(',', '\n'))
+attr = col_name_list
+value1 = issued_last_record
+value2 = used_last_record
+bar = Bar("Bar chart", "Licences usage status in licences server")
+bar.add("issued licences", attr, value1, mark_line=["average"], mark_point=["max", "min"], is_label_show=True,
+        is_datazoom_show=True)
+bar.add("used licences", attr, value2, mark_line=["average"], mark_point=["max", "min"], is_label_show=True,
+        is_datazoom_show=True)
+bar.render()
 
 sender = 'it@xxx.com'
 receiver = 'it@xxx.com'
-subject = 'test subject'
-content = '<html><h1>mail with file test again</h1></html>'
+subject = 'Licences Check'
+content = '<html><h1>Licences usage status in licences server</h1></html>'
 smtpserver = 'xxx-com.mail.protection.outlook.com'
 
 msgRoot = MIMEMultipart('mixed')
@@ -60,22 +56,15 @@ msgRoot['To'] = receiver
 msgText = MIMEText(content, 'html', 'utf-8')
 msgRoot.attach(msgText)
 
-fp = open('/tmp/issued.txt', 'rb')
+fp = open('render.html', 'rb')
 msgAtt = MIMEText(fp.read(), 'base64', 'utf-8')
 msgAtt["Content-Type"] = 'application/octet-stream'
-msgAtt["Content-Disposition"] = 'attachment; filename="issued.txt"'
+msgAtt["Content-Disposition"] = 'attachment; filename="licences_check.html"'
 msgRoot.attach(msgAtt)
-
-fp1 = open('/tmp/used.txt', 'rb')
-msgAtt1 = MIMEText(fp1.read(), 'base64', 'utf-8')
-msgAtt1["Content-Type"] = 'application/octet-stream'
-msgAtt1["Content-Disposition"] = 'attachment; filename="used.txt"'
-msgRoot.attach(msgAtt1)
 
 if __name__ == '__main__':
     smtp = smtplib.SMTP()
     smtp.connect('xxx-com.mail.protection.outlook.com')
     smtp.sendmail(sender, receiver, msgRoot.as_string())
     smtp.quit()
-
 
